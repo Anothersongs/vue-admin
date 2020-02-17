07c29d9205cc77ea82ae7b3c74a250a8e3474fb8
zsm <673658804@qq.com>
2020-02-17 17:43:51 +0800

    初版
    Signed-off-by: 'zsm' <'673658804@qq.com'>

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        这里是登录页
+    </div>
+</template>
+<script>
+export default{
+  name: 'login'
+}
+</script>
+<style scoped lang="scss">
+#login {
+    height:50vh;
+    background-color:rgb(190, 143, 143);
+}
+</style>
