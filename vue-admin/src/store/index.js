2549630199ea8f2fe984ad49d4a2535818f80576
zsm <673658804@qq.com>
2020-04-26 09:17:17 +0800

    zsm

--- vue-admin/src/store/index.js
+++ vue-admin/src/store/index.js
@@ -1,20 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Cookie from "cookie_js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        isCollapse: false,
-        count: 10
+      isCollapse: JSON.parse(sessionStorage.getItem('isCollapse')) || false
     },
     getters: {
       // computed
-      count2: state => state.count + 10
+      isCollapse: state => state.isCollapse
     },
     mutations: {
       SET_COLLAPSE(state){
         state.isCollapse = !state.isCollapse
+        //html5本地存储
+        sessionStorage.setItem('isCollapse', JSON.stringify(state.isCollapse))
       },
       SET_COUNT(state, value){
         state.count = value
